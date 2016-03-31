2cb5591ea73f89e4c3d8b569107b55bdfe692fb4
antoniojimenez <ajv.antonio@gmail.com>
2016-03-31 17:52:43 +0200

    Configuracion openshift

--- config.php
+++ config.php
@@ -1,11 +1,18 @@
 <?php
+if (isset($_ENV['OPENSHIFT_APP_NAME'])) {
+    $db_user=$_ENV['OPENSHIFT_MYSQL_DB_USERNAME']; //Openshift db name OPENSHIFT_MYSQL_DB_USERNAME
+    $db_host=$_ENV['OPENSHIFT_MYSQL_DB_HOST']; //Openshift db host OPENSHIFT_MYSQL_DB_HOST
+    $db_password=$_ENV['OPENSHIFT_MYSQL_DB_PASSWORD']; //Openshift db password OPENSHIFT_MYSQL_DB_PASSWORD
+    $db_name="worldofmusic"; //Openshift db name
+}else{
+	$db_user="root"; //my db user
+    $db_host="localhost"; //my db host
+    $db_password=""; //my db password
+    $db_name="worldofmusic"; //my db name
+  }
 
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$db = "worldofmusic";
-$conectar = mysql_connect($dbhost,$dbuser,$dbpass); 
+$conectar = mysql_connect($db_host,$db_user,$db_pass); 
 mysql_set_charset('utf8', $conectar);
-mysql_select_db($db);
+mysql_select_db($db_name);
 
 ?>
